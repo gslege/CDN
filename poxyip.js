7927067febfaf1ab5e5c5d51ecb23d2b7e79e6f5
GSlege <liaoshuheng@gmail.com>
2025-12-05 18:54:57 +0800

    Update poxyip.js

--- poxyip.js
+++ poxyip.js
@@ -1,7 +1,7 @@
 addEventListener(
   "fetch", event => {
       let url = new URL(event.request.url);
-      url.hostname = "replit.dev域名";                        
+      url.hostname = "change.your.domain"; // 此处单引号里填写你的节点伪装域名 
       url.protocol = "https";
       let request = new Request(url, event.request);
       event.respondWith(
